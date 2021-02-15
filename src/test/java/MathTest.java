645a0a45890dec3397a1ac17a4275fd188225571
drozdi-k <irisiadrozdi@gmail.com>
2021-02-15 18:18:28 +0300

    My first homework

--- src/test/java/MathTest.java
+++ src/test/java/MathTest.java
@@ -1,15 +1,25 @@
-import org.junit.Test;
-
-import junit.framework.TestCase;
+import org.testng.annotations.Test;
+import org.apache.log4j.Logger;
+//import org.apache.log4j.xml.DOMConfigurator;
+//import junit.framework.TestCase;
 import static org.junit.Assert.*;
 
-public class MathTest extends TestCase{
-
+public class MathTest{
+    Logger logger = Logger.getLogger(MathTest.class);
     @Test
     public void subtraction() {
         int a = 18;
         int b = 7;
         assertEquals(11, Math.subtraction(a,b));
+        logger.info("a - b = " + a + " - " + b + " = " + Math.subtraction(a,b));
+    }
+
+    @Test
+    public void antiSubtraction() {
+        int a = 18;
+        int b = 7;
+        logger.info("a - b = " + a + " - " + b + " = " + Math.subtraction(a,b));
+        assertEquals(27, Math.subtraction(a,b));
     }
 
     @Test
@@ -17,6 +27,16 @@ public class MathTest extends TestCase{
         int a = 7;
         int b = 4;
         assertEquals(11,Math.addition(a,b));
+        logger.info("a + b = " + a + " + " + b + " = " + Math.addition(a,b));
+
+    }
+
+    @Test
+    public void antiAddition() {
+        int a = 7;
+        int b = 4;
+        logger.info("a + b = " + a + " + " + b + " = " + Math.addition(a,b));
+        assertEquals(27, Math.addition(a,b));
     }
 
     @Test
@@ -24,6 +44,15 @@ public class MathTest extends TestCase{
         int a = 22;
         int b = 2;
         assertEquals(11,Math.division(a,b));
+        logger.info("a / b = " + a + " / " + b + " = " + Math.division(a,b));
+    }
+
+    @Test
+    public void antiDivision() {
+        int a = 22;
+        int b = 2;
+        logger.info("a / b = " + a + " / " + b + " = " + Math.division(a,b));
+        assertEquals(27, Math.division(a,b));
     }
 
     @Test
@@ -31,5 +60,14 @@ public class MathTest extends TestCase{
         int a = 5;
         int b = 2;
         assertEquals(10,Math.multiplication(a,b));
+        logger.info("a * b = " + a + " * " + b + " = " + Math.multiplication(a,b));
+    }
+
+    @Test
+    public void antiMultiplication() {
+        int a = 22;
+        int b = 2;
+        logger.info("a * b = " + a + " * " + b + " = " + Math.multiplication(a,b));
+        assertEquals(27, Math.multiplication(a,b));
     }
 }
