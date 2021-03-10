5b477127c5537c531799b83202e21f6652e79b35
Irina <78806068+drozdi-k@users.noreply.github.com>
2021-03-10 11:45:02 +0300

    Update PowerCalculator.java

--- src/main/java/Loops/PowerCalculator.java
+++ src/main/java/Loops/PowerCalculator.java
@@ -3,10 +3,13 @@ package Loops;
 public class PowerCalculator {
 
     public static int power(int number, int pow){
-        int result = number, i=2;
-        while(i<=pow){
-            result=result*number;
-            i++;
+        int result = number;
+        if (number >= 0) {
+            int i = 2;
+            while (i <= pow) {
+                result = result * number;
+                i++;
+            }
         }
         return result;
     }
